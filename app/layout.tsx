cb162daa3cda296eca9009d16f019ab1209f2654
Eflex <158610374+digitaleflex@users.noreply.github.com>
2025-07-05 12:16:42 +0100

    app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
 import { ShieldCheck } from "lucide-react";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
